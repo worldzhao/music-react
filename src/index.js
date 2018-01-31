3088f05fcd57b455036ef0e53ea7b16f600b4855
worldzhao <1007326924@qq.com>
2018-01-31 14:51:11 +0800

    add redux-devtools

--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import { BrowserRouter } from 'react-router-dom'
+import { composeWithDevTools } from 'redux-devtools-extension' // eslint-disable-line
 import reducers from './redux/index'
 
 import Dashboard from './views/dashboard/index'
 import './common/style/reset.css'
 import './common/style/fonts.css'
 
-const store = createStore(reducers, applyMiddleware(thunk))
+const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)))
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>

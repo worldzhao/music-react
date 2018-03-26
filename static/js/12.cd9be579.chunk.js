2bc12785c631c66dbe5baaffa5546a0116b7bcca
海秋 <zhaozhiwen@cai-inc.com>
2018-03-26 19:04:55 +0800

    Updates

--- static/js/12.cd9be579.chunk.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([12],{190:function(e,n,t){"use strict";function u(){return a.a.createElement("h1",null,"\u641c\u7d22\u529f\u80fd")}Object.defineProperty(n,"__esModule",{value:!0});var r=t(0),a=t.n(r);n.default=u}});
-//# sourceMappingURL=12.cd9be579.chunk.js.map

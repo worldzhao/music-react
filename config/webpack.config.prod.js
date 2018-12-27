5b566b0d26adb4e0b1d50b484fb204c4b563d1a8
zhaozhiwen <zhaozhiwen@cai-inc.com>
2018-12-27 10:22:45 +0800

    update readme

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -95,6 +95,12 @@ module.exports = {
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
+      '@views': path.join(__dirname, '../src/views'),
+      '@components': path.join(__dirname, '../src/components'),
+      '@router': path.join(__dirname, '../src/router'),
+      '@common': path.join(__dirname, '../src/common'),
+      '@src': path.join(__dirname, '../src'),
+      '@store': path.join(__dirname, '../src/store'),
     },
     plugins: [
       // Prevents users from importing files from outside of src/ (or node_modules/).
